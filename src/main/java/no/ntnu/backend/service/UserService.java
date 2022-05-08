f7d722390b4e60e6612c2804bdba93cc69fa166e
okuma1 <dufzar-sifzi6-jamPyg>
2022-05-08 11:46:11 +0200

    Create UserService with save and get my mail

--- src/main/java/no/ntnu/backend/service/UserService.java
+++ src/main/java/no/ntnu/backend/service/UserService.java
@@ -1,4 +1,42 @@
 package no.ntnu.backend.service;
 
+import no.ntnu.backend.entity.User;
+import no.ntnu.backend.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
 public class UserService {
+
+    private final UserRepository userRepository;
+
+    @Autowired
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    /**
+     * Creates a new user
+     * @param user
+     * @return The user
+     */
+    public User saveUser(User user){
+        user.setPassword(user.getPassword());
+        return userRepository.save(user);
+    }
+
+    /**
+     * Get a specific user by email
+     * @param email
+     * @return user found
+     */
+    public Optional<User> getUserByMail(String email){
+        return userRepository.findById(email);
+    }
+
+
+
+
 }
