4fb8e4da1e181cf92e702f57b80e04295b08d375
okuma1 <dufzar-sifzi6-jamPyg>
2022-05-09 17:16:08 +0200

    Add createUser and getCurrentUser

--- src/main/java/no/ntnu/backend/controller/UserController.java
+++ src/main/java/no/ntnu/backend/controller/UserController.java
@@ -1,17 +1,21 @@
 package no.ntnu.backend.controller;
+import no.ntnu.backend.dto.MessageDTO;
+import no.ntnu.backend.dto.UserDTO;
+import no.ntnu.backend.entity.User;
 import no.ntnu.backend.service.UserService;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.security.Principal;
+import java.util.Optional;
 
 
 @RestController
 @CrossOrigin
 @RequestMapping("/user")
-
-
 public class UserController {
 
     private final UserService userService;
@@ -20,4 +24,40 @@ public class UserController {
     public UserController(UserService userService) {
         this.userService = userService;
     }
+
+    /**
+     * Create a new user
+     * @param data userdata
+     * @return new user
+     */
+    @PostMapping
+    public ResponseEntity<Object> createUser(@RequestBody UserDTO data) {
+        return ResponseEntity.ok(new UserDTO(userService.saveUser(new User(data))));
+    }
+
+    /**
+     * Get the current user
+     * @param principal The user
+     * @return The user
+     */
+    @GetMapping
+    public ResponseEntity<Object> getCurrentUser(Principal principal){
+        Optional<User> optionalUser = userService.getUserByMail(principal.getName());
+        if (optionalUser.isEmpty()){
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(new MessageDTO("User not found"));
+        }
+        return ResponseEntity.ok(new UserDTO(optionalUser.get()));
+    }
+
+
+
+
+
+
+
+
+
+
+
+
 }
