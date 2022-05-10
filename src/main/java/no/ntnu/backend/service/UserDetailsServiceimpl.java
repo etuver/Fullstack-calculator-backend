2e1f1cc3f43c3e9670fe9771f7935476e0437837
okuma1 <dufzar-sifzi6-jamPyg>
2022-05-10 12:26:51 +0200

    Add implementation of UserDetailsService interface

---
+++ src/main/java/no/ntnu/backend/service/UserDetailsServiceimpl.java
@@ -0,0 +1,42 @@
+package no.ntnu.backend.service;
+
+import no.ntnu.backend.entity.User;
+import no.ntnu.backend.repository.UserRepository;
+import no.ntnu.backend.security.UserPrincipal;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
+import java.util.Optional;
+
+
+/**
+ * guide here
+ * https://www.baeldung.com/spring-security-authentication-with-a-database
+ */
+
+@Service
+@Transactional
+public class UserDetailsServiceimpl implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+
+    public UserDetailsServiceimpl(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        Optional<User> user = userRepository.findById(username);
+
+        if (user.isEmpty()){
+            throw new UsernameNotFoundException("User not found");
+        }
+        return UserPrincipal.of(user.get());
+    }
+}
