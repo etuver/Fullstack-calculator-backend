5b1e93f616c112e5dabbd2c9e1124fe93d01e581
okuma1 <dufzar-sifzi6-jamPyg>
2022-05-10 12:28:06 +0200

    Create Controller class for calculation

--- src/main/java/no/ntnu/backend/controller/CalculatorController.java
+++ src/main/java/no/ntnu/backend/controller/CalculatorController.java
@@ -1,4 +1,48 @@
 package no.ntnu.backend.controller;
 
+
+import no.ntnu.backend.dto.ExpressionDTO;
+import no.ntnu.backend.entity.Expression;
+import no.ntnu.backend.service.CalculatorService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.security.Principal;
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
+@RestController
+@RequestMapping("/calculation")
 public class CalculatorController {
+
+    private final CalculatorService calculatorService;
+
+    @Autowired
+    public CalculatorController(CalculatorService calculatorService){
+        this.calculatorService = calculatorService;
+    }
+
+    @PostMapping
+    public ResponseEntity<Object> postCalculation(@RequestBody ExpressionDTO data, Principal principal){
+        Optional<Expression> expression = calculatorService.calculate(data.getExpression(), data.getExpressionUserEmail());
+
+        if (expression.isEmpty()){
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Unable to calculate");
+        }
+        System.out.println("kom hit");
+        return ResponseEntity.ok(new ExpressionDTO(expression.get()));
+    }
+
+    @GetMapping("/user")
+    public ResponseEntity<Object> getCalculations(Principal principal){
+        List<ExpressionDTO> list = calculatorService.getExpressionByUserEmail(principal.getName()).stream()
+                .map(ExpressionDTO::new).collect(Collectors.toList());
+        return ResponseEntity.ok(list);
+    }
+
+
+
 }
