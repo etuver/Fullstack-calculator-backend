aa6ae6f39c433c90237552813438d58166db8984
okuma1 <dufzar-sifzi6-jamPyg>
2022-05-11 19:57:03 +0200

    Make response a string with result

--- src/main/java/no/ntnu/backend/controller/CalculatorController.java
+++ src/main/java/no/ntnu/backend/controller/CalculatorController.java
@@ -3,7 +3,11 @@ package no.ntnu.backend.controller;
 
 import no.ntnu.backend.dto.ExpressionDTO;
 import no.ntnu.backend.entity.Expression;
+import no.ntnu.backend.entity.User;
+import no.ntnu.backend.repository.ExpressionRepository;
+import no.ntnu.backend.repository.UserRepository;
 import no.ntnu.backend.service.CalculatorService;
+import no.ntnu.backend.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -21,11 +25,54 @@ public class CalculatorController {
     private final CalculatorService calculatorService;
 
     @Autowired
+    UserRepository userRepository;
+
+    private UserService userService;
+
+    @Autowired
+    ExpressionRepository expressionRepository;
+
+    @Autowired
     public CalculatorController(CalculatorService calculatorService){
         this.calculatorService = calculatorService;
     }
 
-    @PostMapping
+    @PostMapping("")
+    public ResponseEntity<String> postCalculation(@RequestBody ExpressionDTO data, Principal principal){
+        System.out.println("data: " + data.getExpression() + " " + data.getExpressionUserEmail());
+        if (data.getExpression().isBlank() || data.getExpressionUserEmail().isBlank()){
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Unable to calculate");
+        }
+        String res = calculatorService.calculate(data.getExpression());
+        String fullExp = data.getExpression() + " = " + res;
+        Expression expression = new Expression(data.getExpressionUserEmail(), fullExp );
+        Optional<User> optionalUser = userRepository.findById(data.getExpressionUserEmail());
+        if (optionalUser.isPresent()){
+
+            optionalUser.ifPresent(expression::setUser);
+
+            expressionRepository.save(expression);
+        }
+        System.out.println("res: " + res);
+        return ResponseEntity.ok(res);
+    }
+
+    @GetMapping("/exp/{useremail}")
+    public ResponseEntity<Object> getCalculations(Principal principal){
+        List<ExpressionDTO> list = calculatorService.getExpressionByUserEmail(principal.getName()).stream()
+                .map(ExpressionDTO::new).collect(Collectors.toList());
+        return ResponseEntity.ok(list);
+    }
+
+}
+
+
+    /**
+     *
+     * @param data
+     * @param principal
+     * @return
+    @PostMapping("")
     public ResponseEntity<Object> postCalculation(@RequestBody ExpressionDTO data, Principal principal){
         System.out.println("data: " + data.getExpression() + " " + data.getExpressionUserEmail());
         Optional<Expression> expression = calculatorService.calculate(data.getExpression(), data.getExpressionUserEmail());
@@ -36,14 +83,4 @@ public class CalculatorController {
         System.out.println("kom hit");
         return ResponseEntity.ok(new ExpressionDTO(expression.get()));
     }
-
-    @GetMapping("/user")
-    public ResponseEntity<Object> getCalculations(Principal principal){
-        List<ExpressionDTO> list = calculatorService.getExpressionByUserEmail(principal.getName()).stream()
-                .map(ExpressionDTO::new).collect(Collectors.toList());
-        return ResponseEntity.ok(list);
-    }
-
-
-
-}
+     */
