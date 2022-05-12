4a843c3927668e6fa353339b1d94304cfb4ea8ab
okuma1 <dufzar-sifzi6-jamPyg>
2022-05-12 11:58:42 +0200

    make findAllByExpressionUser return Optional list of expressions instead of list

--- src/main/java/no/ntnu/backend/repository/ExpressionRepository.java
+++ src/main/java/no/ntnu/backend/repository/ExpressionRepository.java
@@ -5,10 +5,13 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface ExpressionRepository extends JpaRepository<Expression, Long> {
 
-    public List<Expression> findAllByEspressionUser(String email);
+    //public List<Expression> findAllByEspressionUser(String email);
+
+    Optional<List<Expression>> findAllByEspressionUser(String email);
 
 }
